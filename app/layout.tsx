a5e83a2060983930e739f740dae17bf1bc112694
OwomeB <153549191+OwomeB@users.noreply.github.com>
2025-05-08 12:18:22 -0500

    First Commit - dashboard creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 text-center">
+          <p className="text-sm">Got that dog in me</p>
+          </footer>
+        </body>
     </html>
   );
 }
